2c4c248d0e7b55fdcc57d65077a9a62e7e09e3bd
LorinLee <lorinlee1996@gmail.com>
2016-12-11 09:35:28 +0800

    Add: StopCommand

---
+++ src/StopCommand.php
@@ -0,0 +1,52 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: lorin
+ * Date: 11/29/16
+ * Time: 6:28 PM
+ */
+
+namespace LorinLee\LaradockCli\Console;
+
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Process\Process;
+
+class StopCommand extends Command
+{
+    /**
+     * Configure the command options
+     *
+     * @return void
+     */
+    protected function configure()
+    {
+        $this
+            ->setName('stop')
+            ->setDescription('Stops all the containers')
+        ;
+    }
+
+    /**
+     * Execute the command
+     *
+     * @param InputInterface $input
+     * @param OutputInterface $output
+     * @return void
+     */
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        $stopCommand = 'cd laradock && docker-compose stop';
+
+        $process = new Process($stopCommand);
+
+        $output->writeln('<info>Stop Containers...</info>');
+
+        $process->run(function ($type, $line) use ($output) {
+            $output->write($line);
+        });
+
+        $output->writeln('<comment>Done.</comment>');
+    }
+}
