9ab6bfb7e033b9c9748c5f3cf103fb85ba9ed048
lilei <lorinlee1996@gmail.com>
2017-02-05 23:43:09 +0800

    Fix: BashCommand

--- src/BashCommand.php
+++ src/BashCommand.php
@@ -13,7 +13,7 @@ use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
 
-class WorkspaceCommand extends Command
+class BashCommand extends Command
 {
     /**
      * Configure the command options
